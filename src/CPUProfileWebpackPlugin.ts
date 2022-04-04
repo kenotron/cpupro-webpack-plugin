8208424f074172387e667c1685792f396ba78227
Ken Chau <kchau@microsoft.com>
2022-04-04 15:28:30 -0700

    simplify the dep chain

--- src/CPUProfileWebpackPlugin.ts
+++ src/CPUProfileWebpackPlugin.ts
@@ -1,7 +1,7 @@
 import { Compiler } from "webpack";
 import path from "path";
-// @ts-ignore TS2307 cpupro uses the package.json exports which TS doesn't quite support yet
-import profiler from "cpupro";
+import { promisify } from "util";
+import inspector from "inspector";
 
 export interface CPUProfileWebpackPluginOptions {
   profileName?: string;
@@ -11,13 +11,18 @@ export interface CPUProfileWebpackPluginOptions {
 const PluginName = "CPUProfileWebpackPlugin";
 
 export class CPUProfileWebpackPlugin {
+  private session: inspector.Session;
   private profileName: string;
 
   constructor(private options: CPUProfileWebpackPluginOptions = {}) {
+    this.session = new inspector.Session();
+    this.session.connect();
     this.profileName = options.profileName ?? "webpack";
   }
 
   apply(compiler: Compiler): void {
+    const fs = compiler.intermediateFileSystem;
+    const writeFile = promisify(fs.writeFile);
     const logger = compiler.getInfrastructureLogger(PluginName);
 
     if (!this.options.outputPath) {
@@ -26,14 +31,31 @@ export class CPUProfileWebpackPlugin {
 
     // Start profiling as soon as this plugin is applied
     logger.info(`Starting CPU Profile: ${this.profileName}`);
-    profiler.profile(this.profileName);
+    const cpuProfilerEnable = promisify(this.session.post.bind(this.session, "Profiler.enable"));
+    const cpuProfilerStart = promisify(this.session.post.bind(this.session, "Profiler.start"));
+    const cpuProfilerStop = promisify(this.session.post.bind(this.session, "Profiler.stop"));
+
+    const profileStartPromise = cpuProfilerEnable().then(() => {
+      return cpuProfilerStart();
+    });
 
     // Stop profiling when the entire webpack run is done
     compiler.hooks.done.tapPromise(PluginName, async (_stats) => {
+      await profileStartPromise;
+
       const { outputPath } = this.options;
-      const profile = profiler.profileEnd(this.profileName);
-      profile.writeToFile(outputPath);
-      logger.info(`CPU Profile written to: ${outputPath}`);
+      try {
+        const profile = await cpuProfilerStop();
+
+        if (!profile) { 
+          throw new Error("output did not contain profile information");
+        }
+        
+        await writeFile(outputPath, JSON.stringify(profile.profile));
+        logger.info(`CPU Profile written to: ${outputPath}`);
+      } catch (e) {
+        logger.error(`CPU Profile plugine encountered an error: ${e}`);
+      }
     });
   }
 }
